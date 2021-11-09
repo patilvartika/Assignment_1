1e76e072391225aaf873c41cd17db58ab0615e55
patilvartika <93961594+patilvartika@users.noreply.github.com>
2021-11-09 17:48:40 +0530

    Add files via upload

---
+++ End the Words To Mirror Dimension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Send the Words To Mirror Dimension
+
+str = input("Enter the string: ")
+print("The reverse string is :", str[::-1])
+
+
+# In[ ]:
+
+
+
+
---
+++ Lets Play With Fibonacci.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+#Lets Play With Fibonacci
+
+a,b=0,1
+
+while b<50:
+    print(b, end=" ")
+    a,b = b,a+b
+
+
+# In[ ]:
+
+
+
+
